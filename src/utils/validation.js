25f2cd9a484b0c8436ac267c2542356a44c4bd64
Kevin Wessa <kjwessa@gmail.com>
2023-07-15 12:24:13 -0500

    chores: add console.logs to validation.js for testing

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,20 +1,24 @@
 import { emailRegex } from "./constants";
 
 const LoginValidation = (email, password) => {
+  console.log("Validating login...");
   return emailRegex.test(email) && password.length >= 4;
 };
 
 const SignUpValidation = (email, password, name) => {
+  console.log("Validating signup...");
   return emailRegex.test(email) && password.length >= 4 && name.length > 2;
 };
 
 const NewItemValidation = (itemName, itemLink, weatherType) => {
+  console.log("Validating new item...");
   return (
     itemName.length > 0 && itemLink.length > 0 && ["hot", "warm", "cold"].includes(weatherType)
   );
 };
 
 const UpdateProfileValidation = (name, avatar) => {
+  console.log("Validating profile update...");
   return name.length > 2 && avatar.length > 2;
 };
 
