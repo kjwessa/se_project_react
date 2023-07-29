04aec3f87fbacc0c96b3f0ef7a0b00ef0dfeffd3
Kevin Wessa <kjwessa@gmail.com>
2023-07-29 14:43:34 -0500

    chores: remove console.logs from Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -24,10 +24,7 @@ export default function Main({
     }
   }
 
-  console.log("Total cards:", cards.length);
-  console.log("Filtering on weather:", weatherType);
   const filteredCards = cards.filter((item) => {
-    console.log("Filtering item:", item);
     return item.weather?.toLowerCase() === weatherType;
   });
 
@@ -51,90 +48,3 @@ export default function Main({
     </main>
   );
 }
-//TODO Submission: remove code below
-// function Main({ currentTemp, skyCondition, cards, onCardClick, onCardLike }) {
-//   const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
-
-//   const weatherType = getWeatherType();
-
-//   const filteredCards = cards.filter((item) => {
-//     return item.weather?.toLowerCase() === weatherType;
-//   });
-
-//   function getWeatherType() {
-//     if (tempObj?.temp?.main >= 86) {
-//       return "hot";
-//     } else if (tempObj?.temp?.main >= 66 && tempObj?.temp?.main <= 85) {
-//       return "warm";
-//     } else if (tempObj?.temp?.main <= 65) {
-//       return "cold";
-//     }
-//   }
-
-// const weatherTempString = String(weatherData[currentTemperatureUnit]).split("°", 2);
-// const weatherTempNumber = weatherTempString[0] || 999;
-// const weatherTempUnit = weatherTempString[1] || "F";
-
-// const getWeatherTypeFareneheit = () => {
-//   if (weatherTempNumber >= 86) {
-//     return "hot";
-//   } else if (weatherTempNumber >= 66 && weatherTempNumber <= 85) {
-//     return "warm";
-//   } else if (weatherTempNumber <= 65) {
-//     return "cold";
-//   }
-// };
-
-// const getWeatherTypeCelsius = () => {
-//   if (weatherTempNumber >= 30) {
-//     return "hot";
-//   } else if (weatherTempNumber >= 18 && weatherTempNumber <= 29) {
-//     return "warm";
-//   } else if (weatherTempNumber <= 17) {
-//     return "cold";
-//   }
-// };
-
-// const getWeatherType = () => {
-//   if (currentTemperatureUnit === "F") {
-//     return getWeatherTypeFareneheit();
-//   }
-//   if (currentTemperatureUnit === "C") {
-//     return getWeatherTypeCelsius();
-//   }
-// };
-
-// const weatherType = getWeatherType();
-
-// const filteredCards = cards.filter((item) => {
-//   return item.weather.toLowerCase() === weatherType;
-// });
-
-//   return (
-//     <main>
-//       <WeatherCard
-//         day={true}
-//         type="clear"
-//         weatherTemp={weatherTempNumber}
-//         weatherUnit={weatherTempUnit}
-//       />
-//       <section className="card-section">
-//         Today is {weatherTempNumber}°{weatherTempUnit} / You may want to wear:
-//         <div className="card-section__items">
-//           {filteredCards.map((card) => {
-//             return (
-//               <ItemCard
-//                 key={card.id}
-//                 card={card}
-//                 onCardClick={onCardClick}
-//                 onCardLike={onCardLike}
-//               />
-//             );
-//           })}
-//         </div>
-//       </section>
-//     </main>
-//   );
-// }
-
-// export default Main;
